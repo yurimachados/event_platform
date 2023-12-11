8dbb7eb5edfda66da2b3f71d1ed2800c14fad8ba
Yuri Machado <ymachado1995@gmail.com>
2023-12-11 16:50:16 -0300

    Add registration form and sign-up functionality

--- core/views.py
+++ core/views.py
@@ -1,7 +1,10 @@
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Event, Comment, Rating, Company
+from django.shortcuts import render, redirect
+from .models import Event, Company
 from django import forms
+from .forms import RegistrationForm
+from django.contrib.auth import login, logout, authenticate
 
 class EventListView(ListView):
     model = Event
@@ -60,3 +63,16 @@ class EventDeleteView(DeleteView):
     model           = Event
     template_name   = 'core/event_delete.html'
     success_url = reverse_lazy('event-list')
+
+
+def sign_up(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/event-list')
+    else:
+        form = RegistrationForm()
+
+    return render(request, 'registration/sign_up.html', {"form": form}) 
